ae2957057a6a7cdd876544af80c5ed81b20317ea
wizardwand <shirishpatil362@gmail.com>
2017-12-17 15:05:55 +0530

    files in css and app will getRefresh automatically

---
+++ gulpProject/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server:{
+            baseDir:'app'
+        }
+    });
+});
+
+gulp.task('watch',['browserSync'], function(){
+    gulp.watch('app/index.html' , browserSync.reload);
+    gulp.watch('app/css/*.css' , browserSync.reload);
+    gulp.watch('app/js/*.js' , browserSync.reload);
+});
+
